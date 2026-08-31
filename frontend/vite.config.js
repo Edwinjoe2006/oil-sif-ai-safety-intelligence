a22bd49a4202efe95167323ddfc61b7321653f63
Edwin joe.M <edwinjoe550@gmail.com>
2026-08-31 18:42:49 +0530

    Connect frontend and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: ' https://oil-sif-ai-safety-backend.onrender.com',
+        target: 'https://oil-sif-ai-safety-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       },
